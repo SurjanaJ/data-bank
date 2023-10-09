0e6f82f5e50c19a92baa452c08785898a14d286d
SurjanaJ <surjana.joshi42@gmail.com>
2023-10-09 13:30:19 +0545

    Revert "create upload links for country meta and unit meta"
    This reverts commit 914e7c626cefe1a232b68e782862b025ccaefcce.

--- databank/import/urls.py
+++ databank/import/urls.py
@@ -1,10 +1,14 @@
 from django.urls import include, path
 from . import views
 urlpatterns = [
+<<<<<<< HEAD
     path('', views.display_trade_table, name='display_trade_table'),
     path('upload_country_meta_excel/', views.upload_country_meta_excel, name='upload_country_meta_excel'),
     path('upload_unit_meta_excel/', views.upload_unit_meta_excel, name='upload_unit_meta_excel'),
     path('upload_hs_code_meta_excel/', views.upload_hs_code_meta_excel, name='upload_hs_code_meta_excel'),
     path('upload_trade_excel/', views.upload_trade_excel, name='upload_trade_excel')
 
+=======
+    path('', views.display_trade_table, name='display_trade_table')
+>>>>>>> parent of 914e7c6 (create upload links for country meta and unit meta)
 ]
