c50a82058954dd4a656b6ea335a7e98d592ce331
SurjanaJ <surjana.joshi42@gmail.com>
2023-10-09 15:29:04 +0545

    create trade data model

--- databank/import/models.py
+++ databank/import/models.py
@@ -28,28 +28,27 @@ class Unit_meta(models.Model):
         return self.Unit_Code
        
 
-# class TradeData(models.Model):
-#     TRADE_OPTIONS = (
-#         ('Import', 'Import'),
-#         ('Export', 'Export')
-#     )
-#     id = models.IntegerField(primary_key=True, unique=True, editable=False)
-#     Trade_Type = models.CharField(max_length=100, choices=TRADE_OPTIONS, null=True, blank=True) 
-#     Calender = models.DateField(null=True, blank=True)
-#     Fiscal_Year = models.CharField(max_length=10, null=True, blank=True)
-#     Month_Duration = models.CharField(max_length=100, null=True, blank=True)
-#     Country = models.ForeignKey(Country_meta, on_delete=models.CASCADE, related_name='tradedata_country')
-#     HS_Code = models.ForeignKey(HS_Code_meta, on_delete=models.CASCADE)
-#     Product_Information = models.TextField()
-#     Unit = models.ForeignKey(Unit_meta, on_delete=models.CASCADE)
-#     Quantity = models.IntegerField(default=0, null=True, blank=True)
-#     Currency_Type = models.CharField(max_length=100, null=True, blank=True)
-#     Amount = models.DecimalField(max_digits=100, decimal_places=3,null=True, blank=True)
-#     Tarrif = models.DecimalField(max_digits=100, decimal_places=3, null=True, blank=True)
-#     Origin_Destination =  models.ForeignKey(Country_meta, on_delete=models.CASCADE, related_name='tradedata_origin_destination')
-#     TradersName_ExporterImporter = models.CharField(max_length=100, null=True, blank=True)
-#     DocumentsLegalProcedural = models.CharField(max_length=300, null=True, blank=True)
+class TradeData(models.Model):
+    TRADE_OPTIONS = (
+        ('Import', 'Import'),
+        ('Export', 'Export')
+    )
+    id = models.IntegerField(primary_key=True, unique=True, editable=False)
+    Trade_Type = models.CharField(max_length=100, choices=TRADE_OPTIONS, null=True, blank=True) 
+    Calender = models.DateField(null=True, blank=True)
+    Fiscal_Year = models.CharField(max_length=10, null=True, blank=True)
+    Month_Duration = models.CharField(max_length=100, null=True, blank=True)
+    Country = models.ForeignKey(Country_meta, on_delete=models.CASCADE, related_name='tradedata_country')
+    HS_Code = models.ForeignKey(HS_Code_meta, on_delete=models.CASCADE)
+    Unit = models.ForeignKey(Unit_meta, on_delete=models.CASCADE)
+    Quantity = models.IntegerField(default=0, null=True, blank=True)
+    Currency_Type = models.CharField(max_length=100, null=True, blank=True)
+    Amount = models.DecimalField(max_digits=100, decimal_places=3,null=True, blank=True)
+    Tarrif = models.DecimalField(max_digits=100, decimal_places=3, null=True, blank=True)
+    Origin_Destination =  models.ForeignKey(Country_meta, on_delete=models.CASCADE, related_name='tradedata_origin_destination')
+    TradersName_ExporterImporter = models.CharField(max_length=100, null=True, blank=True)
+    DocumentsLegalProcedural = models.CharField(max_length=300, null=True, blank=True)
     
 
-#     def __str__(self):
-#         return (str(self.id) + self.Product_Information)
+    def __str__(self):
+        return (str(self.id) + self.Product_Information)
