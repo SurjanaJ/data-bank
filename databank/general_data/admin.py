c1bb039f642e3b0c2aac54e7a447cdfbd94509f0
SurjanaJ <surjana.joshi42@gmail.com>
2023-10-31 13:17:19 +0545

    Feature: Change ForestData admin panel

--- databank/general_data/admin.py
+++ databank/general_data/admin.py
@@ -2,5 +2,8 @@ from django.contrib import admin
 
 from .models import ForestData
 
-# Register your models here.
-admin.site.register(ForestData)
+
+class ForestData_Admin(admin.ModelAdmin ):
+    list_display = ('id', 'Name_Of_The_Plant','Country', 'Area_Unit', 'Area_Covered',  'Stock_Unit', 'Stock_Available')
+
+admin.site.register(ForestData, ForestData_Admin)
