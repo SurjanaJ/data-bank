4cc1dd6f2acf50d94906eaa33c7757e2c10f0302
SurjanaJ <surjana.joshi42@gmail.com>
2023-11-01 15:46:55 +0545

    duplicate data entry for country, unit meta

--- databank/trade_data/views.py
+++ databank/trade_data/views.py
@@ -1,7 +1,9 @@
 import datetime
+from django.db import IntegrityError
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.db.models import Q
+from numpy import NaN
 import pandas as pd
 from django.db.models import Sum
 from django.core.paginator import Paginator, Page
@@ -72,48 +74,83 @@ def display_trade_table(request):
 
     return render(request, 'import/display_trade_table.html', context)
 
-
 def upload_country_meta_excel(request):
+    errors = [] 
     if request.method == 'POST':
         form = UploadCountryMetaForm(request.POST, request.FILES)
         if form.is_valid():
             excel_data = request.FILES['country_meta_file']
+
+            # Read existing data from the database
+            db_data_list = list(Country_meta.objects.values('Country_Name', 'Country_Code_2', 'Country_Code_3'))
+            
             df = pd.read_excel(excel_data)
+            df.replace( NaN, 'nan', inplace=True)
 
+            # records_to_insert = []
             for index, row in df.iterrows():
-                try:
-                    country_meta = Country_meta(
-                    Country_Name=row['Country_Name'],
-                    Country_Code_2=row['Country_Code_2'],
-                    Country_Code_3=row['Country_Code_3']
-                )
-                    country_meta.save()
-                except DataError as e:
-                    print(f"Error inserting row {index}: {e}")
-                    print(f"Problematic row data: {row}")
+                country_data = {
+                    'Country_Name': row['Country_Name'],
+                    'Country_Code_2': row['Country_Code_2'],
+                    'Country_Code_3': row['Country_Code_3']
+                }
 
-            return HttpResponse('success')
+                # Check for duplicates
+                is_duplicate = country_data in db_data_list
+
+                if not is_duplicate:
+                    # Insert the non-duplicate record
+                    try:
+                        country_meta = Country_meta(**country_data)
+                        country_meta.save()
+                    except IntegrityError as e:
+                        print(f"Error inserting row {index}: {e}")
+                        print(f"Problematic row data: {country_data}")
+                else:
+                    errors.append(f"Could not add duplicate row {index}: {country_data}")
+            if errors:
+                # If there are errors, return them as a response
+                return render(request, 'import/error_template.html', {'errors': errors})
+            else:
+                return HttpResponse('success')
     else:
         form = UploadCountryMetaForm()
     return render(request, 'import/upload_form.html', {'form': form})
 
 
 def upload_unit_meta_excel(request):
+    errors = [] 
     if request.method == 'POST':
         form = UploadUnitMetaForm(request.POST, request.FILES)
         if form.is_valid():
             excel_data = request.FILES['unit_meta_file']
+            db_data_list = list(Unit_meta.objects.values('Unit_Code', 'Unit_Name'))
+
             df = pd.read_excel(excel_data)
+            df.replace( NaN, 'nan', inplace=True)
 
             for index, row in df.iterrows():
-                unit_meta = Unit_meta(
-                    Unit_Code=row['Unit_Code'],
-                    Unit_Name=row['Unit_Name']
-                )
+                unit_data = {
+                    'Unit_Name': row['Unit_Name'],
+                    'Unit_Code': row['Unit_Code'],
+                }
+                is_duplicate = unit_data in db_data_list
 
-                unit_meta.save()
-
-            return HttpResponse('success')
+                if not is_duplicate:
+                    # Insert the non-duplicate record
+                    try:
+                        unit_meta = Country_meta(**unit_data)
+                        unit_meta.save()
+                    except IntegrityError as e:
+                        print(f"Error inserting row {index}: {e}")
+                        print(f"Problematic row data: {unit_data}")
+                else:
+                    errors.append(f"Could not add duplicate row {index}: {unit_data}")
+            if errors:
+                # If there are errors, return them as a response
+                return render(request, 'import/error_template.html', {'errors': errors})
+            else:
+                return HttpResponse('success')
     else:
         form = UploadUnitMetaForm()
     return render(request, 'import/upload_form.html', {'form': form})
