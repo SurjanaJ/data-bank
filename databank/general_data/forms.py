5d7846318184d8ab72097cafb15b64c3a5a0ddfb
Sadhan-Shakya <sadhanshakya83@gmail.com>
2023-11-20 13:16:19 +0545

    forest edit checkbox delete

--- databank/general_data/forms.py
+++ databank/general_data/forms.py
@@ -1,5 +1,11 @@
 from django import forms
 from django.forms import ModelForm
+from .models import ForestData
 
 class UploadForestDataForm(forms.Form):
     forest_data_file = forms.FileField()
+
+class UpdateForestData(ModelForm):
+    class Meta:
+        model = ForestData
+        fields='__all__'
--- databank/general_data/urls.py
+++ databank/general_data/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     path('forest', views.upload_forest_excel, name='upload_forest_excel'),
-    path('forest_table', views.display_forest_table, name='forest_table')
+    path('forest_table', views.display_forest_table, name='forest_table'),
+    path('delete_selected/', views.delete_selected_records, name='delete_selected_records'),
+    path('update_forest_record/<int:pk>/',views.update_forest_record,name='update_forest_record')
 ]
