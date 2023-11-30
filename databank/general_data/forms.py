2a506c0ac6d1c82883ba2ea3e33354dfb700f66d
Sadhan-Shakya <sadhanshakya83@gmail.com>
2023-11-30 16:50:40 +0545

    trade table design update

--- databank/general_data/forms.py
+++ databank/general_data/forms.py
@@ -10,4 +10,16 @@ class UploadForestData(ModelForm):
         model = ForestData
         fields='__all__'
 
-        
+
+        widgets={
+            'Year': forms.DateInput(attrs={'class': 'form-control '}),
+            'Country': forms.Select(attrs={'class': 'form-control '}),
+            'Name_Of_The_Plant': forms.TextInput(attrs={'class': 'form-control '}),
+            'Scientific_Name': forms.TextInput(attrs={'class': 'form-control '}),
+            'Local_Name': forms.TextInput(attrs={'class': 'form-control  '}),
+            'Stock_Unit': forms.TextInput(attrs={'class': 'form-control  '}),
+            'Stock_Available': forms.NumberInput(attrs={'class': 'form-control  '}),
+            'Area_Unit': forms.Select(attrs={'class': 'form-control  '}),
+            'Area_Covered': forms.NumberInput(attrs={'class': 'form-control  '}),
+
+        }
