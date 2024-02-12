c93c3b82374fae0ed52cb6844606c97ed2bc6121
SurjanaJ <surjana.joshi42@gmail.com>
2024-02-12 16:45:09 +0545

    update climate table using id

--- databank/general_data/views/climate_view.py
+++ databank/general_data/views/climate_view.py
@@ -1,5 +1,7 @@
 from datetime import date
 from django.db import IntegrityError
+
+from .energy_view import strip_spaces
 from ..forms import UploadClimateForm, UploadClimatePlaceMeta
 from ..models import Climate_Data, Climate_Place_Meta
 from trade_data.models import Country_meta, Unit_meta
@@ -130,103 +132,155 @@ def upload_climate_excel(request):
         if form.is_valid():
             excel_data = request.FILES['file']
             df = pd.read_excel(excel_data)
-            cols = df.columns.tolist()
             df.fillna('', inplace=True)
             df['Date'] = pd.to_datetime(df['Date']).dt.date
+            df = df.map(strip_spaces)
 
-            for index, row in df.iterrows():
-                climate_data = {col: row[col] for col in cols}
-                try:
-                    Date = row['Date']
-                    Country = Country_meta.objects.get(Country_Name = row['Country'])
-                    Place = Climate_Place_Meta.objects.get(Place_Name = row['Place'])
-                    Temperature_Unit = Unit_meta.objects.get(Unit_Code=row['Temperature_Unit'])
-                    Climate_Unit = Unit_meta.objects.get(Unit_Code=row['Climate_Unit'])
-
-                    climate_type = row['Climate']
-                    if climate_type not in ['Rain', 'Snow','Storm']:
-                        raise ValueError(f"Invalid Climate at row {index}: {climate_type}")
-                    
-                    climate_data = {
-                        'Country': Country,
-                        'Date': row['Date'],
-                        'Place':Place,
-                        'Temperature_Unit': Temperature_Unit,
-                        'Max_Temperature': row['Max_Temperature'],
-                        'Min_Temperature': row['Min_Temperature'],
+            # Update the existing data
+            if 'id' in df.columns:
+                for index, row in df.iterrows():
+                    id = row.get('id')
+                    data ={
+                        'Country': row['Country'],
+                        'Date': row['Date'].isoformat(),
+                        'Place':row['Place'],
+                        'Temperature_Unit': row['Temperature Unit'],
+                        'Max_Temperature': row['Max Temperature'],
+                        'Min_Temperature': row['Min Temperature'],
                         'Climate': row['Climate'],
-                        'Climate_Unit' : Climate_Unit,
+                        'Climate_Unit' : row['Climate Unit'],
                         'Amount': row['Amount'],
                     }
-                
-                except Exception as e:
-                    climate_data['Date'] = Date.isoformat()
-                    errors.append({'row_index': index, 'data': climate_data, 'reason': str(e)})      
-                    continue     
+                    try:
+                        climate_instance = Climate_Data.objects.get(id = id)
+                        climate_data = data
 
-                existing_record = Climate_Data.objects.filter(
-                    Q(Country = Country)
-                    & Q(Date = Date)
-                    & Q(Place = Place)
-                ).first()
+                        try:
+                            Date = row['Date']
+                            Country = Country_meta.objects.get(Country_Name = row['Country'])
+                            Place = Climate_Place_Meta.objects.get(Place_Code = row['Place'])
+                            Temperature_Unit = Unit_meta.objects.get(Unit_Code=row['Temperature Unit'])
+                            Climate_Unit = Unit_meta.objects.get(Unit_Code=row['Climate Unit'])
 
-                if existing_record:
-                    existing_dict = model_to_dict(existing_record)
-                    climate_data_dict = model_to_dict(Climate_Data(**climate_data))
+                            climate_type = row['Climate']
+                            if climate_type not in ['Rain', 'Snow','Storm']:
+                                raise ValueError(f"Invalid Climate at row {index}: {climate_type}")
 
-                    if all(existing_dict[key] == climate_data_dict[key] or (pd.isna(existing_dict[key]) and pd.isna(climate_data_dict[key])) for key in climate_data_dict if key != 'id'):
+                            climate_instance.Country = Country
+                            climate_instance.Date= Date
+                            climate_instance.Place = Place
+                            climate_instance.Temperature_Unit = Temperature_Unit
+                            climate_instance.Max_Temperature = row['Max Temperature']
+                            climate_instance.Min_Temperature = row['Min Temperature']
+                            climate_instance.Climate = climate_type
+                            climate_instance.Climate_Unit = Climate_Unit
+                            climate_instance.Amount = row['Amount']
+
+                            climate_instance.save()
+                            updated_count += 1
+                            
+                        # meta data not present
+                        except Exception as e:
+                            climate_data = data
+                            errors.append({'row_index': index, 'data': climate_data, 'reason': str(e)})
+                            continue
+                    
+                    except Exception as e:
+                        climate_data = data
+                        errors.append({
+                                    'row_index': index,
+                                    'data': climate_data,
+                                    'reason': f'Error inserting row {index}: {e}'
+                                })
+                        continue
+            
+            # Add new records
+            else:
+                for index, row in df.iterrows():
+                    data ={
+                        'Country': row['Country'],
+                        'Date': row['Date'].isoformat(),
+                        'Place':row['Place'],
+                        'Temperature_Unit': row['Temperature Unit'],
+                        'Max_Temperature': row['Max Temperature'],
+                        'Min_Temperature': row['Min Temperature'],
+                        'Climate': row['Climate'],
+                        'Climate_Unit' : row['Climate Unit'],
+                        'Amount': row['Amount'],
+                    }
+                    try:
+                        Date = row['Date']
+                        Country = Country_meta.objects.get(Country_Name = row['Country'])
+                        Place = Climate_Place_Meta.objects.get(Place_Code = row['Place'])
+                        Temperature_Unit = Unit_meta.objects.get(Unit_Code=row['Temperature Unit'])
+                        Climate_Unit = Unit_meta.objects.get(Unit_Code=row['Climate Unit'])
+
+                        climate_type = row['Climate']
+                        if climate_type not in ['Rain', 'Snow','Storm']:
+                            raise ValueError(f"Invalid Climate at row {index}: {climate_type}")
+
                         climate_data = {
-                            'Country': Country.Country_Name,
-                            'Date': Date.isoformat(),
-                            'Place':Place.Place_Name,
-                            'Temperature_Unit': Temperature_Unit.Unit_Code,
-                            'Max_Temperature': row['Max_Temperature'],
-                            'Min_Temperature': row['Min_Temperature'],
-                            'Climate': row['Climate'],
-                            'Climate_Unit' : Climate_Unit.Unit_Code,
-                            'Amount': row['Amount'],
+                            'Country': Country,
+                            'Date':Date.isoformat(),
+                            'Place': Place,
+                            'Temperature_Unit' : Temperature_Unit,
+                            'Max_Temperature': row['Max Temperature'],
+                            'Min_Temperature': row['Min Temperature'],
+                            'Climate': climate_type,
+                            'Climate_Unit':Climate_Unit,
+                            'Amount': row['Amount']
                         }
 
-                        duplicate_data.append({
-                            
-                            'row_index': index,
-                            'data': {key: str(climate_data[key]) if isinstance(climate_data[key], date) else climate_data[key] for key in climate_data}
-                        })
-                       
-                    else:
-                        for key, value in climate_data.items():
-                            setattr(existing_record, key, value)
-                            try:
-                                existing_record.save()
-                                updated_count += 1
-                            
-                            except IntegrityError as e:
-                                errors.append(f"Error updating row {index}: {e}")
-                            
-                else:
-                    try:
-                        climateData = Climate_Data(**climate_data)
-                        climateData.save()
-                        added_count += 1
-                    except Exception as e:
-                        errors.append(f"Error inserting row {index}: {e}")
-                        
-            if added_count > 0:
-                messages.success(request, str(added_count) + ' records added.')
-            
-            if updated_count > 0:
-                messages.info(request, str(updated_count) + ' records updated.')
+                        existing_record = Climate_Data.objects.filter(
+                            Q(Country = Country)
+                            & Q(Date = Date)
+                            & Q(Place = Place)
+                            & Q(Temperature_Unit=Temperature_Unit)
+                            & Q(Max_Temperature = row['Max Temperature'])
+                            & Q(Min_Temperature = row['Min Temperature'])
+                            & Q(Climate = climate_type)
+                            & Q(Climate_Unit= Climate_Unit)
+                            & Q(Amount = row['Amount'])
+                        ).first()
 
-            if errors:
-                request.session['errors'] = errors
-                return render(request, 'trade_data/error_template.html', {'errors': errors})
+                        if existing_record:
+                            climate_data= data
+
+                            duplicate_data.append({
+                                'row_index': index,
+                                'data': {key: str(value) for key, value in climate_data.items()}
+                            })
+                            continue
+
+                        else:
+                            try:
+                                ClimateData = Climate_Data(**climate_data)
+                                ClimateData.save()
+                                added_count += 1
+                            except Exception as e:
+                                errors.append(f"Error inserting row {index}: {e}")
+                            
+                    except Exception as e:
+                        climate_data = data
+                        errors.append({'row_index': index, 'data': climate_data, 'reason': str(e)})
+                        continue
+
+        if added_count > 0:
+            messages.success(request, str(added_count) + ' records added.')
             
-            elif duplicate_data:
-                request.session['duplicate_data'] = duplicate_data
-                return render(request, 'trade_data/duplicate_template.html', {'duplicate_data': duplicate_data})
+        if updated_count > 0:
+            messages.info(request, str(updated_count) + ' records updated.')
+
+        if errors:
+            request.session['errors'] = errors
+            return render(request, 'trade_data/error_template.html', {'errors': errors})
+            
+        elif duplicate_data:
+            request.session['duplicate_data'] = duplicate_data
+            return render(request, 'trade_data/duplicate_template.html', {'duplicate_data': duplicate_data})
                 
-            else:
-                return redirect('climate_table') 
+        else:
+            return redirect('climate_table') 
 
     else:
         form = UploadClimateForm()
