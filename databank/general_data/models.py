be38d2f1b5fab15a6b807ca1c8813c4b905f401e
SurjanaJ <surjana.joshi42@gmail.com>
2024-03-01 14:43:05 +0545

    create production meta model

--- databank/general_data/models.py
+++ databank/general_data/models.py
@@ -479,4 +479,13 @@ class Budgetary_Data(models.Model):
 
     def __str__(self):
         return str(self.id)
-    
+
+class Production_Meta(models.Model):
+    id = models.AutoField(primary_key=True)
+    Code = Code=models.CharField(max_length=100)
+    Description = models.TextField()
+    created_date = models.DateTimeField(auto_now=True)
+    modified_date = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return (f'{self.Code} - {self.Description}')
