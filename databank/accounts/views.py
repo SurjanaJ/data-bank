0037803ec357c64a85ef2b8d48943d96deab3c2e
SurjanaJ <surjana.joshi42@gmail.com>
2024-03-06 15:47:34 +0545

    implement register and login feature

--- databank/accounts/views.py
+++ databank/accounts/views.py
@@ -1,9 +1,24 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
 
 from .forms import CreateUserForm
 # Create your views here.
 def loginPage(request):
+
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        
+        user = authenticate(request, username= username, password = password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('display_trade_table')
+        else:
+            messages.info(request, 'username OR password is incorrect.')
+
     context = {}
     return render(request,'accounts/login.html', context)
 
@@ -14,6 +29,8 @@ def registerPage(request):
         form  = CreateUserForm(request.POST)
         if form.is_valid():
             form.save()
+            messages.success(request, 'Account created successfully.')
+            return redirect('login')
 
     context ={'form':form}
     return render(request,'accounts/register.html', context)
