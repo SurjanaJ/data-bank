9f6aa20fa29b960987dc765b305682de56e68a91
SurjanaJ <surjana.joshi42@gmail.com>
2024-03-06 14:39:50 +0545

    create register page

--- databank/accounts/views.py
+++ databank/accounts/views.py
@@ -1,10 +1,19 @@
 from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
 
+from .forms import CreateUserForm
 # Create your views here.
 def loginPage(request):
     context = {}
     return render(request,'accounts/login.html', context)
 
 def registerPage(request):
-    context ={}
+    form = CreateUserForm()
+
+    if request.method == 'POST':
+        form  = CreateUserForm(request.POST)
+        if form.is_valid():
+            form.save()
+
+    context ={'form':form}
     return render(request,'accounts/register.html', context)
